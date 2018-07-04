7e909487a42301eb9023ad6cfebd6696698226d6
FuyaoLi2017 <30902086+FuyaoLi2017@users.noreply.github.com>
2018-07-04 22:39:01 +0800

    July 4th

---
+++ leetcode_347_TopKFrequentElements.java
@@ -0,0 +1,29 @@
+//bucket sort
+class Solution {
+    public List<Integer> topKFrequent(int[] nums, int k) {
+
+	List<Integer>[] bucket = new List[nums.length + 1];
+	Map<Integer, Integer> frequencyMap = new HashMap<Integer, Integer>();
+
+	for (int n : nums) {
+		frequencyMap.put(n, frequencyMap.getOrDefault(n, 0) + 1);
+	}
+
+	for (int key : frequencyMap.keySet()) {   //Returns a Set view of the keys contained in this map.
+		int frequency = frequencyMap.get(key);
+		if (bucket[frequency] == null) {
+			bucket[frequency] = new ArrayList<>();
+		}
+		bucket[frequency].add(key);
+	}
+
+	List<Integer> res = new ArrayList<>();
+
+	for (int pos = bucket.length - 1; pos >= 0 && res.size() < k; pos--) {
+		if (bucket[pos] != null) {
+			res.addAll(bucket[pos]);
+		}
+	}
+	return res;
+    }
+}
