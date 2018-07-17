f25e09d7f52852526a497c6ac53d05057056dd4b
FuyaoLi2017 <1605419584@qq.com>
2018-07-17 14:48:48 +0800

    linked list

---
+++ LinkedList/leetcode_141_LinkedListCycle.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+//HashSet
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+    HashSet<ListNode> nodesSeen = new HashSet<>();
+        while(head != null){
+            if(nodesSeen.contains(head)){
+                return true;
+            }else{
+                nodesSeen.add(head);
+            }
+            head = head.next;
+        }
+        return false;
+    }
+}
+
+ //two pointers
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if(head == null || head.next == null){
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while(fast != slow){
+            if(fast == null || fast.next == null){
+                return false;
+            }
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return true;
+    }
+}
