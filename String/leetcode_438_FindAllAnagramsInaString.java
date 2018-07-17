5c26a6e3384b5b63bb439e9afb50ecc10ec20ca0
FuyaoLi2017 <1605419584@qq.com>
2018-07-17 12:04:14 +0800

    a clearer version

--- String/leetcode_438_FindAllAnagramsInaString.java
+++ String/leetcode_438_FindAllAnagramsInaString.java
@@ -1,27 +1,104 @@
-public List<Integer> findAnagrams(String s, String p) {
-    List<Integer> list = new ArrayList<>();
-    if (s == null || s.length() == 0 || p == null || p.length() == 0) return list;
-    int[] hash = new int[256]; //character hash
-    //record each character in p to hash
-    for (char c : p.toCharArray()) {
-        hash[c]++;
-    }
-    //two points, initialize count to p's length
-    int left = 0, right = 0, count = p.length();
-    while (right < s.length()) {
-        //move right everytime, if the character exists in p's hash, decrease the count
-        //current hash value >= 1 means the character is existing in p
-        if (hash[s.charAt(right++)]-- >= 1) count--;
+public class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+       ///We will use sliding window template
 
-        //when the count is down to 0, means we found the right anagram
-        //then add window's left to result list
-        if (count == 0) list.add(left);
+       ArrayList<Integer> soln = new ArrayList<Integer>();
 
-        //if we find the window's size equals to p, then we have to move left (narrow the window) to find the new match window
-        //++ to reset the hash because we kicked out the left
-        //only increase the count if the character is in p
-        //the count >= 0 indicate it was original in the hash, cuz it won't go below 0
-        if (right - left == p.length() && hash[s.charAt(left++)]++ >= 0) count++;
+       //Check for bad input
+       if (s.length() == 0 || p.length() == 0 || s.length() < p.length()){
+           return new ArrayList<Integer>();
+       }
+
+       //Set up character hash
+       //Keep track of how many times each character appears
+       int[] chars = new int[26];
+       for (Character c : p.toCharArray()){
+           //Increment to setup hash of all characters currently in the window
+           //Later on, these get DECREMENTED when a character is found
+           //A positive count later on means that the character is still "needed" in the anagram
+           //A negative count means that either the character was found more times than necessary
+           //Or that it isn't needed at all
+           chars[c-'a']++;
+       }
+
+       //Start = start poniter, end = end pointer,
+       //len = length of anagram to find
+       //diff = length of currently found anagram. If it equals
+       //the length of anagram to find, it must have been found
+       int start = 0, end = 0, len = p.length(), diff = len;
+
+       char temp;
+       //Before we begin this, the "window" has a length of 0, start and
+       //end pointers both at 0
+       //end 的index在String长度后面一个位置， 跳出循环时候end已经在sliding window的外面一位位置的index
+       for (end = 0; end < len; end++){
+           //Process current char
+           temp = s.charAt(end);
+
+           //As discussed earlier, decrement it
+           chars[temp-'a']--;
+
+           //If it's still >= 0, the anagram still "needed" it so we count it towards the anagram by
+           //decrementing diff (this is saying that this letter is still needed to construct the anagram)
+           if (chars[temp-'a'] >= 0){
+               diff--;
+           }
+       }
+
+       //This would mean that s began with an anagram of p
+       if (diff == 0){
+           soln.add(0);
+       }
+
+       //At this point, start remains at 0, end has moved so that the window is the length of the anagram
+       //from this point on we are going to be moving start AND end on each iteration, to shift the window
+       //along the string
+       //******minus the influence causes by the present first letter and increase the influence caused by next one*****
+       while (end < s.length()){
+
+           //Temp represents the current first character of the window. The character that is
+           //going to be "left behind" as the window moves.
+           temp = s.charAt(start);
+
+           //If it's not negative, this means that the character WAS part of the anagram. That means we
+           //are one step "farther away" from completing an anagram. So we must increment diff.
+           if (chars[temp-'a'] >= 0){
+               diff++;
+           }
+
+           //Increment the hash value for this character, because it is no longer contained in the window
+           chars[temp-'a']++;
+
+           //Increment start to start shifting the window over by 1
+           start++;
+
+           //Temp represents the last character of the window, the "new" character from the window shift.
+           //This character "replaces" the one we removed before so the window stays the same length (p.length())
+           temp = s.charAt(end);
+
+           //Decrement hash value for this character, because it is now a part of the window
+           chars[temp-'a']--;
+
+           //Again, if it's not negative it is part of the anagram. So decrement diff
+           if (chars[temp-'a'] >= 0){
+               diff--;
+           }
+
+           //If diff has reached zero, that means for the last p.length() iterations, diff was decremented and
+           //NOT decremented, which means every one of those characters was in the anagram, so it must be an anagram
+
+           //Note: If many windows in a row find anagrams, then each iteration will have diff incremented then decremented again
+           if (diff == 0){
+               soln.add(start);
+           }
+
+           //Increment for next iteration
+           end++;
+
+       }
+
+       return soln;
+
+
     }
-    return list;
 }
