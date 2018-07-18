aab86cbc22382752aced2011f8da82283d4590d5
FuyaoLi2017 <1605419584@qq.com>
2018-07-19 00:42:47 +0800

    explanations of the algorithm

--- String/leetcode_043_MultiplyStrings.java
+++ String/leetcode_043_MultiplyStrings.java
@@ -2,12 +2,18 @@ public String multiply(String num1, String num2) {
     int m = num1.length(), n = num2.length();
     int[] pos = new int[m + n];
 
+    //loop from higher digits to lower digits
     for(int i = m - 1; i >= 0; i--) {
         for(int j = n - 1; j >= 0; j--) {
+          //multiply to get the result of influence for every digit
             int mul = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
             int p1 = i + j, p2 = i + j + 1;
+            //iterate to update the value of each digit
+            //because we iterate the lower digits first, we should add the influence of previously
+            //calculated lower digits
             int sum = mul + pos[p2];
-
+            ///pos[p1] is the higher digit for the calculated influence of two Integer
+            //pos[p2] is the lower digit for the calculated influence of two Integer
             pos[p1] += sum / 10;
             pos[p2] = (sum) % 10;
         }
