1c6b339e6a23c987ef7b46b02c8807d8ebae33c4
FuyaoLi2017 <1605419584@qq.com>
2018-07-22 00:48:14 +0800

    add brute force solution with memorization

--- TwoPointers/leetcode_209_MinimumSizeSubarraySum.java
+++ TwoPointers/leetcode_209_MinimumSizeSubarraySum.java
@@ -1,3 +1,34 @@
+//Loop with memorization O(n^3)
+class Solution {
+    public int minSubArrayLen(int s, int[] nums) {
+        if(nums.length == 0) return 0;
+        //check all the numbers of the nums
+        int addAll = 0;
+        for(int i = 0; i < nums.length; i++){
+            addAll += nums[i];
+        }
+        if(addAll < s) return 0;
+        int[] sum = new int[nums.length];
+        sum[0] = nums[0];
+        int min = Integer.MAX_VALUE;
+        for(int i = 1; i < nums.length; i++){
+            sum[i] = sum[i - 1] + nums[i];
+        }
+        for(int i = 0; i < nums.length; i++){
+            for(int j = i; j < nums.length; j++){
+                int count;
+                count = sum[j] - sum[i] + nums[i];
+                if(count >= s){
+                    min = Math.min(min, j - i + 1);
+                    break;
+                }
+            }
+        }
+        return min;
+    }
+}
+
+//two pointer solution O(n)
 class Solution {
     public int minSubArrayLen(int s, int[] a) {
       if (a == null || a.length == 0)
