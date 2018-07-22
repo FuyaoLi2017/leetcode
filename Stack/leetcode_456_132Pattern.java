2daf7a7b9ac05a2422eff60d779a9e149902a913
FuyaoLi2017 <1605419584@qq.com>
2018-07-22 18:19:34 +0800

    stack method added

--- Stack/leetcode_456_132Pattern.java
+++ Stack/leetcode_456_132Pattern.java
@@ -1,5 +1,4 @@
 //just array method
-//stack method will be completed afterwards
 public class Solution {
     public boolean find132pattern(int[] nums) {
         int min_i = Integer.MAX_VALUE;
@@ -8,6 +7,33 @@ public class Solution {
             for (int k = j + 1; k < nums.length; k++) {
                 if (nums[k] < nums[j] && min_i < nums[k])
                     return true;
+            }
+        }
+        return false;
+    }
+}
+
+//stack method
+//write down in my notebook
+class Solution {
+    public boolean find132pattern(int[] nums) {
+        if(nums.length < 3)
+            return false;
+        Stack<Integer> stack = new Stack<>();
+        int[] min = new int[nums.length];
+        min[0] = nums[0];
+        for(int i = 1; i < nums.length; i++){
+            min[i] = Math.min(min[i-1], nums[i]);
+        }
+        //no need to look through index at 0, because there must be one number in the front
+        //if not, there won't be any 132 pattern
+        for(int j = nums.length - 1; j > 0; j--){
+            if(nums[j] > min[j]){
+                while(!stack.isEmpty() && stack.peek() <= min[j])
+                    stack.pop();
+                if(!stack.isEmpty() && stack.peek() < nums[j])
+                    return true;
+                stack.push(nums[j]);
             }
         }
         return false;
