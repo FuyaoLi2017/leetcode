ee6f64cfb6c44e0edf94871de45a8967c0b67ef2
FuyaoLi2017 <1605419584@qq.com>
2018-07-29 23:28:42 +0800

    design a structure to encode and decode

---
+++ HashMap/leetcode_535_EncodeAndDecodeTinyURL.java
@@ -0,0 +1,44 @@
+public class Codec {
+    Map<String, String> index = new HashMap<String, String>();
+    Map<String, String> revIndex = new HashMap<String, String>();
+    static String BASE_HOST = "http://tinyurl.com/";
+
+    // Encodes a URL to a shortened URL.
+    public String encode(String longUrl) {
+        if (revIndex.containsKey(longUrl)) return BASE_HOST + revIndex.get(longUrl);
+        String charSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        String key = null;
+        do {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < 6; i++) {
+                int r = (int) (Math.random() * charSet.length());
+                sb.append(charSet.charAt(r));
+            }
+            key = sb.toString();
+        } while (index.containsKey(key));
+        index.put(key, longUrl);
+        revIndex.put(longUrl, key);
+        return BASE_HOST + key;
+    }
+
+    // Decodes a shortened URL to its original URL.
+    public String decode(String shortUrl) {
+        return index.get(shortUrl.replace(BASE_HOST, ""));
+    }
+}
+
+// another method using length of the code
+public class Codec {
+    List<String> urls = new ArrayList<String>();
+    // Encodes a URL to a shortened URL.
+    public String encode(String longUrl) {
+        urls.add(longUrl);
+        return String.valueOf(urls.size()-1);
+    }
+
+    // Decodes a shortened URL to its original URL.
+    public String decode(String shortUrl) {
+        int index = Integer.valueOf(shortUrl);
+        return (index<urls.size())?urls.get(index):"";
+    }
+}
