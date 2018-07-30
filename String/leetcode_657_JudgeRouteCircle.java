8e8bc7f28a6ac84db9d3dc6cab7213da5fcb40b8
FuyaoLi2017 <1605419584@qq.com>
2018-07-30 23:28:15 +0800

    add message

--- String/leetcode_657_JudgeRouteCircle.java
+++ String/leetcode_657_JudgeRouteCircle.java
@@ -1,3 +1,4 @@
+//count every number
 public class Solution {
     public boolean judgeCircle(String moves) {
         int x = 0;
@@ -13,6 +14,32 @@ public class Solution {
 }
 
 //wrong answer why?
+/*
+*by Jitao Zhang
+看半天我才看出来。那个Integer类型在map里声明的时候作为对象。
+java里面好像是-128到127会做缓存也就是提前规定好统一的内存地址分配。
+所以整数在这个范围内你可以用==判断相等。过了这个范围就不行了。你用equals就可以了
+*https://blog.csdn.net/qq_38872310/article/details/79003571
+*/
+//wrong answer
+class Solution{
+	public boolean judgeCircle(String moves){
+		char[] chars = moves.toCharArray();
+		Map<Character, Integer> map = new HashMap<>();
+        for(int i = 0; i < moves.length(); i++){
+        		map.put(chars[i], map.getOrDefault(chars[i], 0) + 1);
+        }
+        if(map.get('R') == map.get('L') && map.get('U') == map.get('D'))
+        	return true;
+        return false;
+	}
+}
+
+//revised version
+/*
+*这样还是不行，因为equals是不能比较null的。所以还是会报错。
+*https://blog.csdn.net/dafeige8/article/details/76719340
+*/
 class Solution{
 	public boolean judgeCircle(String moves){
 		char[] chars = moves.toCharArray();
