228974ca016c4506f15b6c7bf22bc9d890e9ecef
FuyaoLi2017 <1605419584@qq.com>
2018-08-02 23:35:46 +0800

    BST, DFS

---
+++ DFS-BFS/leetcode_513_FindBottomLeftTreeValue.java
@@ -0,0 +1,53 @@
+// DFS
+// use queue to store the element, store the left elements first and it will come out at last
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int findBottomLeftValue(TreeNode root) {
+            Queue<TreeNode> queue = new LinkedList<>();
+    queue.add(root);
+    while (!queue.isEmpty()) {
+        root = queue.poll();
+        if (root.right != null)
+            queue.add(root.right);
+        if (root.left != null)
+            queue.add(root.left);
+    }
+    return root.val;
+    }
+}
+
+// direct DFS, the algorithm is exactly the same
+// keep in mind that this uses a stack for method calls, which essentially is the same using a queue in BFS.
+public class Solution {
+    int ans=0, h=0;
+    public int findBottomLeftValue(TreeNode root) {
+        findBottomLeftValue(root, 1);
+        return ans;
+    }
+    public void findBottomLeftValue(TreeNode root, int depth) {
+        if (h<depth) {ans=root.val;h=depth;}
+        if (root.left!=null) findBottomLeftValue(root.left, depth+1);
+        if (root.right!=null) findBottomLeftValue(root.right, depth+1);
+    }
+}
+
+// the same way with the last one. no global variable method
+public class Solution {
+    public int findBottomLeftValue(TreeNode root) {
+        return findBottomLeftValue(root, 1, new int[]{0,0});
+    }
+    public int findBottomLeftValue(TreeNode root, int depth, int[] res) {
+        if (res[1]<depth) {res[0]=root.val;res[1]=depth;}
+        if (root.left!=null) findBottomLeftValue(root.left, depth+1, res);
+        if (root.right!=null) findBottomLeftValue(root.right, depth+1, res);
+        return res[0];
+    }
+}
