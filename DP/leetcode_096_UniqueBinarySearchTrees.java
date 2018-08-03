984b0c3e0e1017b6f74287f88d0c6425e9ad8a88
FuyaoLi2017 <1605419584@qq.com>
2018-08-03 10:43:46 +0800

    a little change

--- DP/leetcode_096_UniqueBinarySearchTrees.java
+++ DP/leetcode_096_UniqueBinarySearchTrees.java
@@ -8,7 +8,6 @@ class Solution {
         // j is the number of the elements which is smaller than the root,
         // we traverse i by j in the inner loop to find all possiblities in the the problem with scale i
         // we can use dp[j-1] and dp[i-j] to illustrate the scale of the left child side problem and right child side problem
-        // the scale
         for(int i = 2; i <= n; i++){
             for(int j = 1; j <= i; j++){
                 dp[i] += dp[j-1] * dp[i-j];
