7e7f713241d2cee2ff40542957cff882bf39d6f2
FuyaoLi2017 <1605419584@qq.com>
2018-08-03 23:52:13 +0800

    add my own solution

--- Math/leetcode_171_ExcelSheetColumnNumber.java
+++ Math/leetcode_171_ExcelSheetColumnNumber.java
@@ -1,3 +1,18 @@
+// my solution
+class Solution {
+    public int titleToNumber(String s) {
+        char[] chars = s.toCharArray();
+        int length = s.length();
+        int res = 0;
+        for(int i = 0; i < length; i++){
+          // m to the n th power, is Math.pow(m, n)
+            res += (chars[i]-'A'+1) * Math.pow(26, length-i-1);
+        }
+        return res;
+    }
+}
+
+// a concise solution
 class Solution {
     public int titleToNumber(String s) {
         int result = 0;
