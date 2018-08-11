4de609e0c8ddc30671d3f25d3226e029da28c182
FuyaoLi2017 <1605419584@qq.com>
2018-08-11 21:59:20 +0800

    backtracking and swap & recursive method

---
+++ DFS-BFS/leetcode_526_BeautifulArrangement.java
@@ -0,0 +1,90 @@
+// #1 method
+// the worst solution is loop all the possible conditions in a brute force manner
+// check the condition and use recursion method to call the swap function for every digit
+public class Solution {
+    int count = 0;
+    public int countArrangement(int N) {
+        int[] nums = new int[N];
+        for (int i = 1; i <= N; i++)
+            nums[i - 1] = i;
+        permute(nums, 0);
+        return count;
+    }
+    public void permute(int[] nums, int l) {
+        if (l == nums.length - 1) {
+            int i;
+            for (i = 1; i <= nums.length; i++) {
+                if (nums[i - 1] % i != 0 && i % nums[i - 1] != 0)
+                    break;
+            }
+            if (i == nums.length + 1) {
+                count++;
+            }
+        }
+        for (int i = l; i < nums.length; i++) {
+            swap(nums, i, l);
+            permute(nums, l + 1);
+            swap(nums, i, l);
+        }
+    }
+    public void swap(int[] nums, int x, int y) {
+        int temp = nums[x];
+        nums[x] = nums[y];
+        nums[y] = temp;
+    }
+}
+
+// #2 method
+// better brute force, keep checking whether the permutation is valid.
+// this method still swap the actual letters, if we use backingtracking, we can simply develop
+// the tree of the array element by element, which is more concise
+public class Solution {
+    int count = 0;
+    public int countArrangement(int N) {
+        int[] nums = new int[N];
+        for (int i = 1; i <= N; i++)
+            nums[i - 1] = i;
+        permute(nums, 0);
+        return count;
+    }
+    public void permute(int[] nums, int l) {
+        if (l == nums.length) {
+            count++;
+        }
+        for (int i = l; i < nums.length; i++) {
+            swap(nums, i, l);
+            if (nums[l] % (l + 1) == 0 || (l + 1) % nums[l] == 0)
+                permute(nums, l + 1); // if the if sentence can loop to the deepest layer, in the next permute, count will increase 1
+            swap(nums, i, l);
+        }
+    }
+    public void swap(int[] nums, int x, int y) {
+        int temp = nums[x];
+        nums[x] = nums[y];
+        nums[y] = temp;
+    }
+}
+
+// #3 method
+// backingtracking
+public class Solution {
+    int count = 0;
+    public int countArrangement(int N) {
+        boolean[] visited = new boolean[N + 1];
+        calculate(N, 1, visited);
+        return count;
+    }
+    public void calculate(int N, int pos, boolean[] visited) {
+        if (pos > N)
+            count++;
+        for (int i = 1; i <= N; i++) {
+            if (!visited[i] && (pos % i == 0 || i % pos == 0)) {
+              // we must check whether the visited[i] == 0, because we don't need
+              // to recheck the element we have already put in the array
+                visited[i] = true;
+                calculate(N, pos + 1, visited);
+                visited[i] = false;
+            }
+        }
+    }
+}
