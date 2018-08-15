4abaec5fa9bdf85b418f05acb7a9d2e0fb1d5316
FuyaoLi2017 <1605419584@qq.com>
2018-08-16 00:48:03 +0800

    add explanation

--- Stack/leetcode_636_ExclusiveTimeofFunctions.java
+++ Stack/leetcode_636_ExclusiveTimeofFunctions.java
@@ -2,6 +2,7 @@ public class Solution {
     public int[] exclusiveTime(int n, List < String > logs) {
         Stack < Integer > stack = new Stack < > ();
         int[] res = new int[n];
+        // seperate the elements to arrays by split()
         String[] s = logs.get(0).split(":");
         stack.push(Integer.parseInt(s[0]));
         int i = 1, prev = Integer.parseInt(s[2]);
@@ -13,6 +14,8 @@ public class Solution {
                 stack.push(Integer.parseInt(s[0]));
                 prev = Integer.parseInt(s[2]);
             } else {
+              // update the value of corresponding function_id
+              // the ID of the end minus the start ID of the latest period plus 1
                 res[stack.peek()] += Integer.parseInt(s[2]) - prev + 1;
                 stack.pop();
                 prev = Integer.parseInt(s[2]) + 1;
