8114a1f494bc9918be27374f93915fb056dda1eb
FuyaoLi2017 <1605419584@qq.com>
2018-08-16 00:47:34 +0800

    explanation of Greedy method

--- Greedy/leetcode_670_MaximumSwap.java
+++ Greedy/leetcode_670_MaximumSwap.java
@@ -1,11 +1,13 @@
 class Solution {
     public int maximumSwap(int num) {
+      // toString() & toCharArray()
         char[] A = Integer.toString(num).toCharArray();
         int[] last = new int[10];
         for (int i = 0; i < A.length; i++) {
             last[A[i] - '0'] = i;
         }
-
+// we choose to swap the the greatest value in the front to get the biggest number
+// from the start to the end, we check it for the biggest value from 9 to 0
         for (int i = 0; i < A.length; i++) {
             for (int d = 9; d > A[i] - '0'; d--) {
                 if (last[d] > i) {
