b1f088e6a8d2bb812a3d31f4afe6dc0441376b2e
FuyaoLi2017 <1605419584@qq.com>
2018-08-16 23:16:48 +0800

    String

---
+++ String/leetcode_696_CountBinarySubstrings.java
@@ -0,0 +1,39 @@
+// Group by character
+class Solution {
+    public int countBinarySubstrings(String s) {
+        int[] groups = new int[s.length()];
+        int t = 0;
+        groups[0] = 1;
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i-1) != s.charAt(i)) {
+                groups[++t] = 1;
+            } else {
+                groups[t]++;
+            }
+        }
+
+        int ans = 0;
+        for (int i = 1; i <= t; i++) {
+            ans += Math.min(groups[i-1], groups[i]);
+        }
+        return ans;
+    }
+}
+// one pass linear scan
+class Solution {
+public int countBinarySubstrings(String s) {
+if (s == null || s.length() == 0) return 0;
+        int preRun = 0;
+        int curRun =1;
+        int count = 0;
+        for (int i = 1; i < s.length(); i++){
+            if (s.charAt(i) == s.charAt(i-1)) curRun++;
+            else {
+                preRun = curRun;
+                curRun = 1;
+            }
+            if (preRun >= curRun) count++;
+        }
+        return count;
+    }
+}
