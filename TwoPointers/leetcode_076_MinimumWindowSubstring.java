e86af45069fc0edde73d6cd3cf4fe83590771b19
FuyaoLi2017 <1605419584@qq.com>
2018-08-19 01:28:54 +0800

    add my understandings

--- TwoPointers/leetcode_076_MinimumWindowSubstring.java
+++ TwoPointers/leetcode_076_MinimumWindowSubstring.java
@@ -15,12 +15,17 @@ public String minWindow(String s, String t) {
     int minLeft = 0;
     int minLen = s.length()+1;
     int count = 0;
+    // outside loop traverse all possiblities of String s with start value at index 0
+    // count is used to ensure all the letters are included in the range of the chosen string
     for(int right = 0; right < s.length(); right++){
         if(map.containsKey(s.charAt(right))){
             map.put(s.charAt(right),map.get(s.charAt(right))-1);
             if(map.get(s.charAt(right)) >= 0){
                 count ++;
             }
+            // in the while loop, the inner if part shift the left edge of the string
+            // if the string still contains enough letters the hold all string t letters, it will stay in the while and undate the minlen
+            // or, the code might jump out of the while loop to increase the right edge to find a potential smaller minlen
             while(count == t.length()){
                 if(right-left+1 < minLen){
                     minLeft = left;
@@ -40,7 +45,7 @@ public String minWindow(String s, String t) {
     {
         return "";
     }
-
+    // return the index of the desired substring
     return s.substring(minLeft,minLeft+minLen);
 }
 }
