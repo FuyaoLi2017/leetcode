76f8cbfb4b7a92ce5d7522a979e4f08d1c4f251d
FuyaoLi2017 <1605419584@qq.com>
2018-08-18 14:23:33 +0800

    add my solutions

--- TwoPointers/leetcode_125_ValidPalindrome.java
+++ TwoPointers/leetcode_125_ValidPalindrome.java
@@ -1,3 +1,27 @@
+// my Solution
+class Solution {
+    public boolean isPalindrome(String s) {
+        char[] chars = s.toLowerCase().toCharArray();
+        List<Character> list = new ArrayList<Character>();
+        for(char element : chars){
+            if((element - 'a' >= 0 && element - 'a' <= 25) || (element - '0' >= 0 && element - '0' <= 9))
+                list.add(element);
+        }
+        int startIndex = 0;
+        int endIndex = list.size() - 1;
+        while(startIndex < endIndex){
+            if(list.get(startIndex) == list.get(endIndex)){
+                startIndex++;
+                endIndex--;
+            }else{
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+// high vote
 public class Solution {
     public boolean isPalindrome(String s) {
         if (s.isEmpty()) {
@@ -22,5 +46,14 @@ public class Solution {
         }
 
         return true;
+    }
+}
+
+// using reg
+public class Solution {
+    public boolean isPalindrome(String s) {
+        String actual = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
+        String rev = new StringBuffer(actual).reverse().toString();
+        return actual.equals(rev);
     }
 }
