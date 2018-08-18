07824b29292fdf6a57d301ed63f60e7e3e4be0d1
FuyaoLi2017 <1605419584@qq.com>
2018-08-18 14:22:59 +0800

    Binary Search, bit manipulation

--- Binary Search/leetcode_029_DividTwoIntegers.java
+++ Binary Search/leetcode_029_DividTwoIntegers.java
@@ -1,4 +1,30 @@
-public int divide(int dividend, int divisor) {
+// 这道题利用了二分查找的思想，由于不能使用乘除法，所以利用bit manipulation,将得到的数位左移实现二分查找
+// 这个题目可以设置2层while循环，可以减去二分查找的最大模块，然后在剩下的比较小的一块再做二分累加查找
+// 根据：https://leetcode.com/problems/divide-two-integers/discuss/13407/Detailed-Explained-8ms-C++-solution 整理得到的java解法
+class Solution {
+    public int divide(int dividend, int divisor) {
+        if(dividend == Integer.MIN_VALUE && divisor == -1)
+            return Integer.MAX_VALUE;
+        int sign = ((dividend < 0) ^ (divisor < 0)) ? -1 : 1;
+        long dvd = Math.abs((long) dividend);
+	    long dvs = Math.abs((long) divisor);
+        int res = 0;
+        while (dvd >= dvs) {
+            long temp = dvs, multiple = 1;
+            while (dvd >= (temp << 1)) {
+                temp <<= 1;
+                multiple <<= 1;
+            }
+            dvd -= temp;
+            res += multiple;
+        }
+        return sign == 1 ? res : -res;
+    }
+}
+
+// high vote answer in Java
+// using recursive calls to replace the outside layer while loop  while [(dvd >= (temp << 1))]
+ublic int divide(int dividend, int divisor) {
 	//Reduce the problem to positive long integer to make it easier.
 	//Use long to avoid integer overflow cases.
 	int sign = 1;
