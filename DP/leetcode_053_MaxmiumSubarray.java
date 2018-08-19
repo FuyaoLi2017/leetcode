50df66d8972da1931b6656d3242683fcb6c7522a
FuyaoLi2017 <1605419584@qq.com>
2018-08-19 22:41:13 +0800

    add divid and conquer solution

--- DP/leetcode_053_MaxmiumSubarray.java
+++ DP/leetcode_053_MaxmiumSubarray.java
@@ -1,20 +1,3 @@
-// DP solution
-class Solution {
-public int maxSubArray(int[] A) {
-        int n = A.length;
-        int[] dp = new int[n];//dp[i] means the maximum subarray ending with A[i];
-        dp[0] = A[0];
-        int max = dp[0];
-
-        for(int i = 1; i < n; i++){
-            dp[i] = A[i] + (dp[i - 1] > 0 ? dp[i - 1] : 0);
-            // update the result, compare with the previous maximum value, in this way to keep track of the maxmium value
-            max = Math.max(max, dp[i]);
-        }
-
-        return max;
-}
-}
 class Solution {
     public int maxSubArray(int[] nums) {
         int n = nums.length;
@@ -29,5 +12,34 @@ class Solution {
             max = Math.max(max, dp[i]);
         }
         return max;
+    }
+}
+
+// Divid and Conquer
+class Solution {
+    public int maxSubArray(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        return maxSubArray(nums, 0, nums.length-1);
+    }
+
+    private int maxSubArray(int[] nums, int left, int right) {
+        if (left > right) return Integer.MIN_VALUE;
+        if (left == right) return nums[left];
+        int mid = (left + right) / 2;
+        int L = maxSubArray(nums, left, mid-1);
+        int R = maxSubArray(nums, mid+1, right);
+        int leftSum = 0;
+        int tmp = 0;
+        for (int i=mid-1; i>=left; i--) {
+            tmp += nums[i];
+            if (tmp > leftSum) leftSum = tmp;
+        }
+        tmp = 0;
+        int rightSum = 0;
+        for (int i=mid+1; i<=right; i++) {
+            tmp += nums[i];
+            if (tmp > rightSum) rightSum = tmp;
+        }
+        return Math.max(Math.max(L, R), leftSum + rightSum + nums[mid]);
     }
 }
