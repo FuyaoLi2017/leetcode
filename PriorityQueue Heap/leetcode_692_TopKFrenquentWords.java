5afa634b0b5ea8ec7eff440a99d3a5510098f63c
FuyaoLi2017 <1605419584@qq.com>
2018-08-19 22:41:46 +0800

    sort(lambda expression), priority queue

---
+++ PriorityQueue Heap/leetcode_692_TopKFrenquentWords.java
@@ -0,0 +1,38 @@
+// introduction to lambda expression in JDK1.8
+// http://www.importnew.com/15259.html
+class Solution {
+    public List<String> topKFrequent(String[] words, int k) {
+        Map<String, Integer> count = new HashMap();
+        for (String word: words) {
+            count.put(word, count.getOrDefault(word, 0) + 1);
+        }
+        List<String> candidates = new ArrayList(count.keySet());
+        Collections.sort(candidates, (w1, w2) -> count.get(w1).equals(count.get(w2)) ?
+                w1.compareTo(w2) : count.get(w2) - count.get(w1));
+
+        return candidates.subList(0, k);
+      }
+
+// Priority Queue, Heap
+// 学习一下这个数据结构
+class Solution {
+    public List<String> topKFrequent(String[] words, int k) {
+        Map<String, Integer> count = new HashMap();
+        for (String word: words) {
+            count.put(word, count.getOrDefault(word, 0) + 1);
+        }
+        PriorityQueue<String> heap = new PriorityQueue<String>(
+                (w1, w2) -> count.get(w1).equals(count.get(w2)) ?
+                w2.compareTo(w1) : count.get(w1) - count.get(w2) );
+
+        for (String word: count.keySet()) {
+            heap.offer(word);
+            if (heap.size() > k) heap.poll();
+        }
+
+        List<String> ans = new ArrayList();
+        while (!heap.isEmpty()) ans.add(heap.poll());
+        Collections.reverse(ans);
+        return ans;
+    }
+}
