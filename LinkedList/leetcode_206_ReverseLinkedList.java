09a4cf007ca4856ff8054655cd1f1e1d9d41bc93
FuyaoLi2017 <1605419584@qq.com>
2018-08-19 20:42:23 +0800

    Iterative and recursive ways to reverse a linked list

---
+++ LinkedList/leetcode_206_ReverseLinkedList.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+//Iterative method
+// using a prev to point at the null at first
+// prev move along the linked list to reverse each pointing relationship
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while(curr != null){
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+
+        }
+        return prev;
+    }
+}
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+public ListNode reverseList(ListNode head) {
+    /* recursive solution */
+    return reverseListInt(head, null);
+}
+
+private ListNode reverseListInt(ListNode head, ListNode newHead) {
+    if (head == null)
+        return newHead;
+    ListNode next = head.next;
+    head.next = newHead;
+    return reverseListInt(next, head);
+}
+}
