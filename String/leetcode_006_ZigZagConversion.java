a757fe701bf46e922f8e7e952f008cc0aed371a3
FuyaoLi2017 <1605419584@qq.com>
2018-08-22 19:05:22 +0800

    String, StringBuilder, append row by row OR append the certain indexs of the string, one - pass solution

---
+++ String/leetcode_006_ZigZagConversion.java
@@ -0,0 +1,52 @@
+// sort by row
+// use StringBuilder, append characters to the Strings
+class Solution {
+    public String convert(String s, int numRows) {
+
+        if (numRows == 1) return s;
+
+        List<StringBuilder> rows = new ArrayList<>();
+        for (int i = 0; i < Math.min(numRows, s.length()); i++)
+            rows.add(new StringBuilder());
+
+        int curRow = 0;
+        boolean goingDown = false;
+
+        for (char c : s.toCharArray()) {
+            rows.get(curRow).append(c);
+            if (curRow == 0 || curRow == numRows - 1) goingDown = !goingDown;
+            curRow += goingDown ? 1 : -1;
+        }
+
+        StringBuilder ret = new StringBuilder();
+        for (StringBuilder row : rows) ret.append(row);
+        return ret.toString();
+    }
+}
+
+/**
+* Visit row by row method
+* k = 0,1,2,3...
+* Characters in row 0: k*(2*numRows-2)
+* Characters in row numRows-1: k*(2*numRows-2)+numRows+i
+* Characters in the ith row: k*(2*numRows-2)+i or (k+1)*(2*numRows-2)-i
+*/
+class Solution {
+    public String convert(String s, int numRows) {
+
+        if (numRows == 1) return s;
+
+        StringBuilder ret = new StringBuilder();
+        int n = s.length();
+        int cycleLen = 2 * numRows - 2;
+
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j + i < n; j += cycleLen) {
+                ret.append(s.charAt(j + i));
+                if (i != 0 && i != numRows - 1 && j + cycleLen - i < n)
+                    ret.append(s.charAt(j + cycleLen - i));
+            }
+        }
+        return ret.toString();
+    }
+}
