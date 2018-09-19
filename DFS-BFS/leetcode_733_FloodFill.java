b666bc1f242c2434d143a10ba2d369e40780ef58
FuyaoLi2017 <1605419584@qq.com>
2018-09-19 00:35:42 -0700

    update

--- DFS-BFS/leetcode_733_FloodFill.java
+++ DFS-BFS/leetcode_733_FloodFill.java
@@ -1,3 +1,4 @@
+// general DFS solution
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
         if (image[sr][sc] == newColor) return image;
@@ -12,5 +13,23 @@ class Solution {
         fill(image, sr - 1, sc, color, newColor);
         fill(image, sr, sc + 1, color, newColor);
         fill(image, sr, sc - 1, color, newColor);
+    }
+}
+
+// similar solution
+class Solution {
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        int color = image[sr][sc];
+        if (color != newColor) dfs(image, sr, sc, color, newColor);
+        return image;
+    }
+    public void dfs(int[][] image, int r, int c, int color, int newColor) {
+        if (image[r][c] == color) {
+            image[r][c] = newColor;
+            if (r >= 1) dfs(image, r-1, c, color, newColor);
+            if (c >= 1) dfs(image, r, c-1, color, newColor);
+            if (r+1 < image.length) dfs(image, r+1, c, color, newColor);
+            if (c+1 < image[0].length) dfs(image, r, c+1, color, newColor);
+        }
     }
 }
