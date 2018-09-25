2327cc772ca49f9e4d49b87b5f6b8bab70bb6aab
FuyaoLi2017 <1605419584@qq.com>
2018-09-25 11:04:00 -0700

    update

---
+++ TwoPointers/README.md
@@ -0,0 +1,2 @@
+# 1. Sliding window经典题目
+> LC76
--- TwoPointers/leetcode_076_MinimumWindowSubstring.java
+++ TwoPointers/leetcode_076_MinimumWindowSubstring.java
@@ -1,3 +1,59 @@
+/*
+Given a string S and a string T, find the minimum window in S which will contain all the characters in T in complexity O(n).
+
+Example:
+
+Input: S = "ADOBECODEBANC", T = "ABC"
+Output: "BANC"
+Note:
+
+If there is no such window in S that covers all characters in T, return the empty string "".
+If there is such window, you are guaranteed that there will always be only one unique minimum window in S.
+*/
+// 经典的sliding window
+class Solution {
+    public String minWindow(String s, String t) {
+        if (s.length() < t.length()) return "";
+        int slow = 0, fast = 0, index = 0, minLen = Integer.MAX_VALUE, matchCount = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        for (char ch : t.toCharArray()) {
+            Integer count = map.get(ch);
+            if (count == null) map.put(ch, 1);
+            else map.put(ch, count + 1);
+        }
+        for (; fast < s.length(); fast++) {
+            // update the map and matchCount
+            Integer count = map.get(s.charAt(fast));
+            if (count == null) {
+                continue;
+            }
+            map.put(s.charAt(fast), count - 1);
+            if (count == 1) matchCount++;
+
+            // find the the possible minlength
+            while (matchCount == map.size()) {
+                // update the minLength
+                if (fast - slow + 1 < minLen) {
+                    minLen = fast - slow + 1;
+                    index = slow;
+                }
+
+                Integer leftmost = map.get(s.charAt(slow++));
+                if (leftmost == null) {
+                    // slow pointer has moved to the next
+                    continue;
+                } else {
+                    map.put(s.charAt(slow - 1), leftmost + 1);
+                }
+                // break from the loop
+                if (leftmost == 0) matchCount--;
+            }
+        }
+        return minLen == Integer.MAX_VALUE ? "" : s.substring(index, index + minLen);
+    }
+}
+
+// 答案2
 public class Solution {
 public String minWindow(String s, String t) {
     if(s == null || s.length() < t.length() || s.length() == 0){
