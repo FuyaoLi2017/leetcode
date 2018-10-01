d236ceead7b6d4562a6f84d543c9fec59472e9af
FuyaoLi2017 <1605419584@qq.com>
2018-10-01 13:00:08 -0700

    update

--- Array/leetcode_289_GameOfLife.java
+++ Array/leetcode_289_GameOfLife.java
@@ -1,12 +1,47 @@
+/*
+According to the Wikipedia's article: "The Game of Life, also known simply as Life, is a cellular automaton devised by the British mathematician John Horton Conway in 1970."
+
+Given a board with m by n cells, each cell has an initial state live (1) or dead (0). Each cell interacts with its eight neighbors (horizontal, vertical, diagonal) using the following four rules (taken from the above Wikipedia article):
+
+Any live cell with fewer than two live neighbors dies, as if caused by under-population.
+Any live cell with two or three live neighbors lives on to the next generation.
+Any live cell with more than three live neighbors dies, as if by over-population..
+Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
+Write a function to compute the next state (after one update) of the board given its current state. The next state is created by applying the above rules simultaneously to every cell in the current state, where births and deaths occur simultaneously.
+
+Example:
+
+Input:
+[
+  [0,1,0],
+  [0,0,1],
+  [1,1,1],
+  [0,0,0]
+]
+Output:
+[
+  [0,0,0],
+  [1,0,1],
+  [0,1,1],
+  [0,1,0]
+]
+Follow up:
+
+Could you solve it in-place? Remember that the board needs to be updated at the same time: You cannot update some cells first and then use their updated values to update other cells.
+In this question, we represent the board using a 2D array. In principle, the board is infinite, which would cause problems when the active area encroaches the border of the array. How would you address these problems?
+*/
+
 class Solution {
     public void gameOfLife(int[][] board) {
     if (board == null || board.length == 0) return;
+    // keep the board's width
     int m = board.length, n = board[0].length;
 
     for (int i = 0; i < m; i++) {
         for (int j = 0; j < n; j++) {
             int lives = liveNeighbors(board, m, n, i, j);
-
+            // 前面的对数组的修改会影响后面的结果，可以开辟空间，也可以实现inplace
+            // 把第二个bit认为是0，然后给他设置值，需要设置1的有两种情况
             // In the beginning, every 2nd bit is 0;
             // So we only need to care about when will the 2nd bit become 1.
             if (board[i][j] == 1 && lives >= 2 && lives <= 3) {
