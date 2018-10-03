bcb50cde1e0e712b2c5b1c66cda86649052f2af7
FuyaoLi2017 <1605419584@qq.com>
2018-10-03 00:36:36 -0700

    tricky two sides multiply

---
+++ Array/leetcode_238_ProductofArrayExceptSelf.java
@@ -0,0 +1,28 @@
+/*
+Given an array nums of n integers where n > 1,  return an array output such that output[i] is equal to the product of all the elements of nums except nums[i].
+
+Example:
+
+Input:  [1,2,3,4]
+Output: [24,12,8,6]
+Note: Please solve it without division and in O(n).
+
+Follow up:
+Could you solve it with constant space complexity? (The output array does not count as extra space for the purpose of space complexity analysis.)
+*/
+// traverse from right to left and then use temp to the other size's product, multiply them together
+// the current value nums[i] will be ignored
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+    int[] result = new int[nums.length];
+    for (int i = 0, tmp = 1; i < nums.length; i++) {
+        result[i] = tmp;
+        tmp *= nums[i];
+    }
+    for (int i = nums.length - 1, tmp = 1; i >= 0; i--) {
+        result[i] *= tmp;
+        tmp *= nums[i];
+    }
+    return result;
+}
+}
