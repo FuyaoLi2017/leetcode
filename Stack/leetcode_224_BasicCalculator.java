0d2ca381f40ea644309024d3a918a2ce42f71ade
FuyaoLi2017 <1605419584@qq.com>
2018-10-14 15:53:05 -0700

    stack

--- Stack/leetcode_224_BasicCalculator.java
+++ Stack/leetcode_224_BasicCalculator.java
@@ -1,8 +1,40 @@
+/*
+Implement a basic calculator to evaluate a simple expression string.
+
+The expression string may contain open ( and closing parentheses ), the plus + or minus sign -, non-negative integers and empty spaces .
+
+Example 1:
+
+Input: "1 + 1"
+Output: 2
+Example 2:
+
+Input: " 2-1 + 2 "
+Output: 3
+Example 3:
+
+Input: "(1+(4+5+2)-3)+(6+8)"
+Output: 23
+Note:
+You may assume that the given expression is always valid.
+Do not use the eval built-in library function.
+*/
+/*
+Simple iterative solution by identifying characters one by one. One important thing is that the input is valid, which means the parentheses are always paired and in order.
+Only 5 possible input we need to pay attention:
+
+digit: it should be one digit from the current number
+'+': number is over, we can add the previous number and start a new number
+'-': same as above
+'(': push the previous result and the sign into the stack, set result to 0, just calculate the new result within the parenthesis.
+')': pop out the top two numbers from stack, first one is the sign before this pair of parenthesis, second is the temporary result before this pair of parenthesis. We add them together.
+Finally if there is only one number, from the above solution, we haven't add the number to the result, so we do a check see if the number is zero.
+*/
 public int calculate(String s) {
     Stack<Integer> stack = new Stack<Integer>();
     int result = 0;
     int number = 0;
-    int sign = 1;
+    int sign = 1; //sign的作用：记录符号，在后一个数字进来的时候乘上这个sign
     for(int i = 0; i < s.length(); i++){
         char c = s.charAt(i);
         if(Character.isDigit(c)){
@@ -33,3 +65,6 @@ public int calculate(String s) {
     if(number != 0) result += sign * number;
     return result;
 }
+
+// (1+(4+5+2)-3)+(6+8)
+// 如果有多余的空格的话会直接跳过,什么都不执行
