98465aaff443edda10ee45861b1acffa843b5721
FuyaoLi2017 <1605419584@qq.com>
2018-12-18 00:41:57 -0800

    update

--- LinkedList/leetcode_234_PalindromeLinkedList.java
+++ LinkedList/leetcode_234_PalindromeLinkedList.java
@@ -13,4 +13,43 @@ Follow up:
 Could you do it in O(n) time and O(1) space?
 */
 
-// the most intuitive way is to use a stack to hold the linked list
+// the most intuitive way is to use a stack to hold the linked list/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if (head == null)
+            return true;  // uncertain
+        ListNode slow = head, fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        // judge whether it has odd number of list nodes
+        if(fast.next == null) {
+            slow = slow.next;
+        }
+        // push the nodes into a stack
+        Deque<ListNode> stack = new ArrayDeque<>();
+        while (slow != null) {
+            stack.offerFirst(slow);
+            slow = slow.next;
+        }
+        // check whether it is the same like the previous
+        while (!stack.isEmpty()) {
+            if(head.val == stack.pollFirst().val) {
+                head = head.next;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+// a BETTER way, O(n) space and O(1) time
