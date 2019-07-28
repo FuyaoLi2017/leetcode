92f239dac3965e6ef57c95a06376656ba51320a8
FuyaoLi2017 <1605419584@qq.com>
2019-07-28 16:51:35 +0800

    update

---
+++ DFS/leetcode_529_MineSweeper.java
@@ -0,0 +1,57 @@
+/*
+Let's play the minesweeper game (Wikipedia, online game)!
+
+You are given a 2D char matrix representing the game board. 'M' represents an unrevealed mine, 'E' represents an unrevealed empty square, 'B' represents a revealed blank square that has no adjacent (above, below, left, right, and all 4 diagonals) mines, digit ('1' to '8') represents how many mines are adjacent to this revealed square, and finally 'X' represents a revealed mine.
+
+Now given the next click position (row and column indices) among all the unrevealed squares ('M' or 'E'), return the board after revealing this position according to the following rules:
+
+If a mine ('M') is revealed, then the game is over - change it to 'X'.
+If an empty square ('E') with no adjacent mines is revealed, then change it to revealed blank ('B') and all of its adjacent unrevealed squares should be revealed recursively.
+If an empty square ('E') with at least one adjacent mine is revealed, then change it to a digit ('1' to '8') representing the number of adjacent mines.
+Return the board when no more squares will be revealed.
+Note:
+
+The range of the input matrix's height and width is [1,50].
+The click position will only be an unrevealed square ('M' or 'E'), which also means the input board contains at least one clickable square.
+The input board won't be a stage when game is over (some mines have been revealed).
+For simplicity, not mentioned rules should be ignored in this problem. For example, you don't need to reveal all the unrevealed mines when the game is over, consider any cases that you will win the game or flag any squares.
+*/
+
+class Solution {
+    public char[][] updateBoard(char[][] board, int[] click) {
+        // classic dfs
+        int m = board.length, n = board[0].length;
+        int row = click[0], col = click[1];
+
+        if (board[row][col] == 'M') {
+            board[row][col] = 'X';
+        } else {
+            int count = 0;
+            for (int i = -1; i < 2; i++) {
+                for (int j = -1; j < 2; j++) {
+                    if (i == 0 && j == 0) continue;
+                    int r = row + i, c = col + j;
+                    if (r < 0 || r >= m || c < 0 || c >= n) continue;
+                    if (board[r][c] == 'M' || board[r][c] == 'X') count++;
+                }
+            }
+
+            if (count > 0) {
+                board[row][col] = (char)('0' + count);
+            } else {
+                board[row][col] = 'B';
+                for (int i = -1; i < 2; i++) {
+                    for (int j = -1; j < 2; j++) {
+                        // typical dfs with trimming
+                        if (i == 0 && j == 0) continue;
+                        int r = row + i, c = col + j;
+                        if (r < 0 || r >= m || c < 0 || c >= n) continue;
+                        // recusion is called when the position is 'E'
+                        if (board[r][c] == 'E') updateBoard(board, new int[]{r, c});
+                    }
+                }
+            }
+        }
+        return board;
+    }
+}
